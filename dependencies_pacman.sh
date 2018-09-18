62d84cbd6e107be1f460b341974769fabc72a3d6
Nathaniel Clark <pcwizzy37@gmail.com>
2018-09-18 12:55:33 -0700

    Added a script to install dependencies for pacman. (Used in Arch Linux, and derivatives, Manjaro, for example.)

---
+++ dependencies_pacman.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+#Precompiled dependencies
+sudo pacman -S git cmake pkgconf boost boost-libs mercurial freetype2 freeimage zziplib ois nvidia-cg-toolkit \
+openal libxt libxaw libxrandr openssl openssl-1.0 curl gtk2 wxgtk2 wxgtk3 alsa-lib libpulse wget subversion
